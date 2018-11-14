8a0ed7e2f2ecbeb9814862fc47ea995c6f57d137
AaronStahley <acstahle@asu.edu>
2018-11-13 20:58:03 -0700

    US77 - Started to build out a card view for the home screen.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,31 +1,73 @@
 import React from 'react';
-import {ScrollView, StyleSheet, Text, View,Button} from 'react-native';
+import {ScrollView, StyleSheet, Text, View, Button, SectionList, FlatList, Image} from 'react-native';
+import {Card} from 'react-native-elements'
 import { ExpoLinksView } from '@expo/samples';
 import HeaderStyle from '../components/HeaderStyle';
+import AlgorithmBox from "../components/AlgorithmBox";
+import Colors from "../constants/Colors";
 
 
 export default class HomeScreen extends React.Component {
     static navigationOptions = {
-        headerStyle: {
-            borderBottomWidth: 0,
-            backgroundColor: '#fff', elevation:0
-        }
+        headerTitle: (<Image
+            style={{
+                flex: 1,
+                width: 40,
+                height: 40,
+                resizeMode: 'contain',
+                alignSelf: 'center',
+            }}
 
+            source={require('../assets/images/WHITE_HAND_LOGO.png')}/>),
+        headerStyle: {
+            backgroundColor: Colors.navBarBackground,
+            paddingBottom: 8,
+        }
     };
+
     render() {
         const { navigate } = this.props.navigation;
+
         return (
 
         <ScrollView style={styles.container}>
           <View style={styles.welcomeContainer}>
-              <Text style={styles.titleText}>
-                  Welcome What are we doing today?
-              </Text>
-              <Button
-                  onPress={() => navigate('Questions')}
-                  title="BLSI Algorithm"
-                  color= '#ee3e41'
-              />
+              <Card
+                  title='NOM BLSI'>
+                  <Text style={{marginBottom: 10}}>
+                      Algorithm Description Algorithm Description Algorithm Description Algorithm Description
+                      Algorithm Description Algorithm Description Algorithm Description Algorithm Description
+                  </Text>
+                  <Button
+                      onPress={() => navigate('Questions')}
+                      color= '#ee3e41'
+                      buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+                      title='Start' />
+              </Card>
+              <Card
+                  title='Other Algorithm'>
+                  <Text style={{marginBottom: 10}}>
+                      Algorithm Description Algorithm Description Algorithm Description Algorithm Description
+                      Algorithm Description Algorithm Description Algorithm Description Algorithm Description
+                  </Text>
+                  <Button
+                      onPress={() => navigate('Questions')}
+                      color= '#ee3e41'
+                      buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+                      title='Start' />
+              </Card>
+              <Card
+                  title='Other Algorithm'>
+                  <Text style={{marginBottom: 10}}>
+                      Algorithm Description Algorithm Description Algorithm Description Algorithm Description
+                      Algorithm Description Algorithm Description Algorithm Description Algorithm Description
+                  </Text>
+                  <Button
+                      onPress={() => navigate('Questions')}
+                      color= '#ee3e41'
+                      buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+                      title='Start' />
+              </Card>
           </View>
       </ScrollView>
     );
