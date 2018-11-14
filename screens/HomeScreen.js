ce20b19786e6cbf8f2c99abd25f5d8b59fbe384b
cagloria <33134245+cagloria@users.noreply.github.com>
2018-11-13 19:48:26 -0700

    Added Learn More button to algorithm selection screen, added algorithm container style

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {ScrollView, StyleSheet, Text, View,Button} from 'react-native';
+import {ScrollView, StyleSheet, Text, View, Button} from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 import HeaderStyle from '../components/HeaderStyle';
 
@@ -17,19 +17,27 @@ export default class HomeScreen extends React.Component {
         return (
 
         <ScrollView style={styles.container}>
-          <View style={styles.welcomeContainer}>
-              <Text style={styles.titleText}>
-                  Welcome What are we doing today?
-              </Text>
-              <Button
-                  onPress={() => navigate('Questions')}
-                  title="BLSI Algorithm"
-                  color= '#ee3e41'
-              />
-          </View>
-      </ScrollView>
-    );
-  }
+			<View style={styles.welcomeContainer}>
+				<Text style={styles.titleText}>
+					Welcome. What are we doing today?
+				</Text>
+
+				<View style={styles.algorithmContainer}>
+					<Button
+						onPress={() => navigate('Questions')}
+						title="BLSI Algorithm"
+						color= '#ee3e41'
+					/>
+					<Button
+						onPress={() => navigate('AlgorithmDesc')}
+						title='Learn More'
+						color='#9e9e9e'
+					/>
+				</View>
+			</View>
+		</ScrollView>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
@@ -52,6 +60,17 @@ const styles = StyleSheet.create({
         height: 45,
         borderWidth: 0,
         borderRadius: 5
-    }
+    },
+    algorithmContainer: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		width: 300,
+		padding: 15,
+		marginBottom: 10,
+		borderWidth: 1,
+		borderRadius: 5,
+		borderColor: '#ccc',
+		backgroundColor: '#f2f2f2'
+	}
 });
 
