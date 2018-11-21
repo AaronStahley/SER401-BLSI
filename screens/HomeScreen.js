36c31ae74aad844980c8aef11570fd11a5b15201
AaronStahley <acstahle@asu.edu>
2018-11-20 17:18:22 -0700

    sprint4/US77/Task100 - Added custom buttons to algorithm cards and changed styling

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {ScrollView, StyleSheet, Text, View, Button, SectionList, FlatList, Image} from 'react-native';
+import {ScrollView, StyleSheet, Text, View, Button, TouchableOpacity,SectionList, FlatList, Image} from 'react-native';
 import {Card} from 'react-native-elements'
 import { ExpoLinksView } from '@expo/samples';
 import AlgorithmBox from "../components/AlgorithmBox";
@@ -12,21 +12,28 @@ export default class HomeScreen extends React.Component {
         const { navigate } = this.props.navigation;
 
         return (
-
+            /**
+             * TODO Need to move all of this into its own component so that
+             * TODO Data from the model can be accepted into the header and body.
+             *
+             * Styling is mostly done/ Created custom buttons for the cards.
+             */
         <ScrollView style={styles.container}>
           <View >
-              <Card
-                  title='NOM BLSI'>
-                  <Text style={{marginBottom: 10}}>
+              <Card title='NOM BLSI'>
+                  <Text style={styles.bodyText}>
                       Nonoperative management of blunt liver and spleen injury
                       in children (NOMBLSI) helps prevent unnecessary surgeries in children when
                       there may or may not be internal bleeding.
                   </Text>
-                  <Button
-                      onPress={() => this.props.navigation.navigate('Conversation')}
-                      color= '#ee3e41'
-                      buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-                      title='Start' />
+                  <View style={styles.buttonContiner}>
+                      <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate('Conversation')}>
+                          <Text style={styles.buttonText}>Info</Text>
+                      </TouchableOpacity>
+                      <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate('Conversation')}>
+                          <Text style={styles.buttonText}>Start</Text>
+                      </TouchableOpacity>
+                  </View>
               </Card>
               <Card
                   title='Other Algorithm'>
@@ -34,11 +41,14 @@ export default class HomeScreen extends React.Component {
                       Algorithm Description Algorithm Description Algorithm Description Algorithm Description
                       Algorithm Description Algorithm Description Algorithm Description Algorithm Description
                   </Text>
-                  <Button
-                      onPress={() => navigate('Questions')}
-                      color= '#ee3e41'
-                      buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-                      title='Start' />
+                  <View style={styles.buttonContiner}>
+                      <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate('Conversation')}>
+                          <Text style={styles.buttonText}>Info</Text>
+                      </TouchableOpacity>
+                      <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate('Conversation')}>
+                          <Text style={styles.buttonText}>Start</Text>
+                      </TouchableOpacity>
+                  </View>
               </Card>
               <Card
                   title='Other Algorithm'>
@@ -46,11 +56,14 @@ export default class HomeScreen extends React.Component {
                       Algorithm Description Algorithm Description Algorithm Description Algorithm Description
                       Algorithm Description Algorithm Description Algorithm Description Algorithm Description
                   </Text>
-                  <Button
-                      onPress={() => navigate('Questions')}
-                      color= '#ee3e41'
-                      buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-                      title='Start' />
+                  <View style={styles.buttonContiner}>
+                      <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate('Conversation')}>
+                          <Text style={styles.buttonText}>Info</Text>
+                      </TouchableOpacity>
+                      <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate('Conversation')}>
+                          <Text style={styles.buttonText}>Start</Text>
+                      </TouchableOpacity>
+                  </View>
               </Card>
           </View>
       </ScrollView>
@@ -68,11 +81,24 @@ const styles = StyleSheet.create({
         marginBottom: 30,
     },
     button: {
+        flex: 1,
         backgroundColor: '#ee3e41',
-        width: 300,
-        height: 45,
         borderWidth: 0,
-        borderRadius: 5
+        borderRadius: 5,
+        alignItems: 'center',
+        margin: 5
+    },
+    buttonText:{
+        color: '#fff',
+        fontSize: 18,
+        margin: 5
+    },
+    buttonContiner:{
+        flexDirection: 'row',
+
+    },bodyText:{
+        fontSize: 16,
+        marginBottom: 15
     }
 });
 
