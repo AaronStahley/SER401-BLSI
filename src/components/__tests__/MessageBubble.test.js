3886de3bb01b39da350a138476762f3c63ba546f
Taylor Greeff <tgreeff@asu.edu>
2019-01-10 00:23:25 -0700

    Removed files, as they were not used. Had to remove new test, as it was part of removed files. Added test for ProceedButton.

--- src/components/__tests__/MessageBubble.test.js
+++
@@ -1,8 +0,0 @@
-import 'react-native';
-import React from 'react';
-import MessageBubble from '../MessageBubble';s
-import renderer from 'react-test-renderer';
-
-test('renders correctly', () => {
-    const tree = renderer.create(<MessageBubble></MessageBubble>)
-});
