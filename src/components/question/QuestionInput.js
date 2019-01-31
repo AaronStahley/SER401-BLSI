ff8e7ad5cf378ea3e443477ba593b5a650c042c1
AaronStahley <acstahle@asu.edu>
2019-01-30 21:04:56 -0700

    Task197 - Went back to the material UI drop down. It works at the moment but the drop down menu displays the id values and not the labels

--- src/components/question/QuestionInput.js
+++ src/components/question/QuestionInput.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import {View, StyleSheet, Picker} from 'react-native';
+import {View, StyleSheet,Text,  Picker, ActionSheetIOS,TouchableOpacity} from 'react-native';
+import ModalSelector from 'react-native-modal-selector'
+import ModalDropdown from 'react-native-modal-dropdown';
 import CheckBox from './CheckBox';
 import Colors from '../../common/Colors';
 import {Col, Grid} from "react-native-easy-grid";
 import {observer} from "mobx-react/native";
 import NumberField from "../ui/NumberField";
+import { Dropdown } from 'react-native-material-dropdown';
+
+
+
 
 @observer
 export default class QuestionInput extends React.Component {
 
     handleSelection = (selectedOptionId) => {
+
+
         this.props.question.Answer.QuestionOptionId = selectedOptionId;
     };
 
@@ -29,9 +37,40 @@ export default class QuestionInput extends React.Component {
         }
     };
 
+    showActionSheet = () => {
+        let {question} = this.props;
+
+        ActionSheetIOS.showActionSheetWithOptions({
+                options: question.Options.label,
+                cancelButtonIndex: CANCEL_INDEX,
+                destructiveButtonIndex: DESTRUCTIVE_INDEX,
+            },
+            (buttonIndex) => {
+                this.setState({ clicked: BUTTONS[buttonIndex] });
+            });
+    };
 
     render() {
         let {question} = this.props;
+
+        let index = 0;
+        const data = [
+            { key: index++, section: true, label: 'Fruits' },
+            { key: index++, label: 'Red Apples' },
+            { key: index++, label: 'Cherries' },
+            { key: index++, label: 'Cranberries' },
+            { key: index++, label: 'Pink Grapefruit' },
+            { key: index++, label: 'Raspberries' },
+            { key: index++, section: true, label: 'Vegetables' },
+            { key: index++, label: 'Beets' },
+            { key: index++, label: 'Red Peppers' },
+            { key: index++, label: 'Radishes' },
+            { key: index++, label: 'Radicchio' },
+            { key: index++, label: 'Red Onions' },
+            { key: index++, label: 'Red Potatoes' },
+            { key: index++, label: 'Rhubarb' },
+            { key: index++, label: 'Tomatoes' }
+        ];
 
         switch (question.TypeKey) {
             case  "binary":
@@ -53,25 +92,46 @@ export default class QuestionInput extends React.Component {
             case  "select":
                 return (
                     <View style={styles.pickerWrapper}>
-                        <Picker
-                            mode={"dropdown"}
-                            style={styles.picker}
-                            selectedValue={question.Answer.QuestionOptionId}
-                            onValueChange={this.handleSelection}>
-                            <Picker.Item
-                                label={"Select A Value..."}
-                                value={null}
-                            />
-                            {
-                                question.Options.map(option => (
-                                    <Picker.Item
-                                        key={option.Id}
-                                        label={option.Label}
-                                        value={option.Id}
-                                    />
-                                ))
-                            }
-                        </Picker>
+
+                        {/*Example of a modal selector looks not great but may function. */}
+                        {/*<ModalSelector*/}
+                            {/*data={question.Options.map(option => ({key: option.id, label:  option.Label}))}*/}
+                            {/*initValue="Select Item"*/}
+                            {/*onChange={this.handleSelection} />*/}
+
+                        {/*Material UI still looks the best just am having some trouble getting it to work correctly. */}
+                        <Dropdown
+                            label='Select A Value...'
+                            data={question.Options.map(option => ({value: option.Id}))}
+                            onChangeText={this.handleSelection}
+                        />
+
+
+                        {/*Example of a modal Drop down also looks kind tacky.  */}
+                        {/*<ModalDropdown options={['option 1', 'option 2']}/>*/}
+
+
+                        {/* Old picker that needs to be replaced. */}
+                        {/*<Picker*/}
+                            {/*mode={"dropdown"}*/}
+                            {/*style={styles.picker}*/}
+                            {/*selectedValue={question.Answer.QuestionOptionId}*/}
+                            {/*onValueChange={this.handleSelection}>*/}
+                            {/*<Picker.Item*/}
+                                {/*label={"Select A Value..."}*/}
+                                {/*value={null}*/}
+                            {/*/>*/}
+                            {/*{*/}
+                                {/*question.Options.map(option => (*/}
+                                    {/*<Picker.Item*/}
+                                        {/*key={option.Id}*/}
+                                        {/*label={option.Label}*/}
+                                        {/*value={option.Id}*/}
+                                    {/*/>*/}
+                                {/*))*/}
+                            {/*}*/}
+                        {/*</Picker>*/}
+
                     </View>
                 );
             default:
@@ -84,6 +144,8 @@ export default class QuestionInput extends React.Component {
                     />
                 );
         }
+
+
     }
 }
 
