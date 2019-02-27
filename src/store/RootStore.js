4226b76a2c06b1391ee755211dffe3a35224e4b4
Taylor Greeff <tgreeff@asu.edu>
2019-02-26 20:16:18 -0700

    Added StateStores for questions and recommendations to make it easier to insert data into those stores

--- src/store/RootStore.js
+++ src/store/RootStore.js
@@ -6,6 +6,8 @@ import Transporter from "../common/Transporter";
 import QuestionAnswerStore from "./QuestionAnswerStore";
 import QuestionOptionStore from "./QuestionOptionStore";
 import UpdateStore from './UpdateStore';
+import StateQuestionStore from './StateQuestionStore';
+import StateRecommendationStore from './StateRecommendationStore';
 
 export default class RootStore {
     transporter;
@@ -16,14 +18,16 @@ export default class RootStore {
     updateStore;
 
     constructor(dbName) {
-        this.transporter         = new Transporter(dbName);
-        this.algorithmStore      = new AlgorithmStore(this, this.transporter);
-        this.questionStore       = new QuestionStore(this, this.transporter);
-        this.questionAnswerStore = new QuestionAnswerStore(this, this.transporter);
-        this.questionOptionStore = new QuestionOptionStore(this, this.transporter);
-        this.recommendationStore = new RecommendationStore(this, this.transporter);
-        this.stateStore          = new StateStore(this, this.transporter);
-        this.updateStore         = new UpdateStore(this, this.transporter);
+        this.transporter            = new Transporter(dbName);
+        this.algorithmStore         = new AlgorithmStore(this, this.transporter);
+        this.questionStore          = new QuestionStore(this, this.transporter);
+        this.questionAnswerStore    = new QuestionAnswerStore(this, this.transporter);
+        this.questionOptionStore    = new QuestionOptionStore(this, this.transporter);
+        this.recommendationStore    = new RecommendationStore(this, this.transporter);
+        this.stateStore             = new StateStore(this, this.transporter);
+        this.stateQuestionStore     = new StateQuestionStore(this, this.transporter);
+        this.stateRecommendationStore = new StateRecommendationStore(this, this.transporter);
+        this.updateStore            = new UpdateStore(this, this.transporter);
     }
 
     init() {
