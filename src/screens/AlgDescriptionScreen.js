3c0530f5870ec81a5f9e371965e18eaa6c3437ae
Conrad Morawski <cjmoraws@asu.edu>
2019-03-22 23:19:59 -0700

    Dev button for accessing discharge screen

--- src/screens/AlgDescriptionScreen.js
+++ src/screens/AlgDescriptionScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import HTMLView from 'react-native-htmlview';
 import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import RefreshButton from "../components/ui/RefreshButton.js"
+import {Button} from 'react-native';
 
 export default class AlgDescriptionScreen extends React.Component {
     
@@ -21,6 +22,14 @@ export default class AlgDescriptionScreen extends React.Component {
                 <View>
                     <Text style={styles.titleText}>{algorithm.Name}</Text>
                     <HTMLView style={styles.descriptionText} value={`<div>${algorithm.Description}</div>`}/>
+                    <Card
+                        containerStyle={{padding: 30}}>
+                            <Button
+                                onPress={() => this.props.navigation.navigate('DischargeScreen')}
+                                color='#b3b3b3'
+                                buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+                                title='Discharge Instructions'/>
+                    </Card>
                 </View>
             </ScrollView>
         );
@@ -37,6 +46,13 @@ const styles = StyleSheet.create({
         marginTop   : 10,
         marginBottom: 10,
         fontSize    : 30,
+    },
+    button: {
+        backgroundColor: '#ee3e41',
+        width: 300,
+        height: 45,
+        borderWidth: 0,
+        borderRadius: 5
     },
     descriptionText: {
         paddingHorizontal: 20,
