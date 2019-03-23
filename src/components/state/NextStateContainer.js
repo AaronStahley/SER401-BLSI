cfd02445a333f2d41b23c86f661695b8a58dc919
cagloria <33134245+cagloria@users.noreply.github.com>
2019-03-22 18:48:26 -0700

    Added styling to recommendation to tell if the algorithm finishes in a good or bad state

--- src/components/state/NextStateContainer.js
+++ src/components/state/NextStateContainer.js
@@ -17,17 +17,6 @@ export default class NextStateContainer extends React.Component {
         this.setState({
             currentState: rootStore.stateStore.createInstance(nextStateId, path)
         });
-
-        //console.log(nextStateId + ', ' + path);
-        
-        var pathStr = path + '';
-        var pathArr = pathStr.split(':');
-        var prevStateId = pathArr[pathArr.length - 1];
-        pathArr.pop();
-        var prevPath = pathArr.join(':');
-        //console.log(prevStateId + ', ' + prevPath);
-
-        var prevState = rootStore.stateStore.createInstance(prevStateId, prevPath);
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
