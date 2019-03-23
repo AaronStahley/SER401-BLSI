633c4dfe4ef0233a78708565394cf023a7086ad9
Taylor Greeff <tgreeff@asu.edu>
2019-03-23 01:41:07 -0700

    Working on figuring out how to test with SQLite. Not much working.

---
+++ src/model/__tests__/Question.test.js
@@ -0,0 +1,26 @@
+import RootStore from "../../store/root/RootStore";
+
+describe('test question', () => {
+    
+    let root;
+
+    /*
+    beforeAll(() => {
+        root = new RootStore('../../assets/db/database.db')
+    })
+
+    
+    test("retrieval", () => {
+        let question;
+        root.questionStore.getOrFindAll()
+            .then(res => {
+                question = res;
+            })
+        expect(question).toBeDefined();
+    })*/
+
+    test("placholder", () => {
+        let val = true;
+        expect(val).toBeTruthy()
+    })
+})
