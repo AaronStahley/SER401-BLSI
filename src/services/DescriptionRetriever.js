6251b16025c4310914c29a733cfcb64828389cd4
Taylor Greeff <tgreeff@asu.edu>
2019-03-29 09:00:36 -0700

    Added start for usage of google drive apis for images.

---
+++ src/services/DescriptionRetriever.js
@@ -0,0 +1,11 @@
+
+export default class GoogleDriveSender {
+    //https://developers.google.com/drive/api/v3/quickstart/nodejs
+    constructor() {
+
+    }
+
+    listData(path = "")
+    
+    }
+}
